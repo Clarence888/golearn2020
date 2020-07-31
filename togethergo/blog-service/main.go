2997a8e3fef44775ee7596108de792a70199797a
Clarence <changliang@xiaozhu.com>
2020-07-31 14:47:40 +0800

    博客项目 简单接入成功

--- togethergo/blog-service/main.go
+++ togethergo/blog-service/main.go
@@ -1,14 +1,28 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"blog-service/internal/routers"
+	"net/http"
+	"time"
+)
 
 func main()  {
+	/*
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200,gin.H{"message":"pong"})
 	})
 	r.Run(":9999")
-
+	 */
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr: ":9999",
+		Handler: router,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
 }
 
 /*
